375ab8d047b73f63e9988dca56500e498103ef51
Andrii Zyma <yozhikvtumane52@gmail.com>
2026-03-04 20:16:07 +0200

    що змінилось:

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ ax.set(title="Some random data, created with JupyterLab!")
 
 # build a simple line graph 
 x = np.linspace(0, 2, 10)  # Sample data.
-
+# ya zdyvovanyi
 plt.figure(figsize=(5, 2.7), layout='constrained')
 plt.plot(x, x, label='linear')  # Plot some data on the (implicit) Axes.
 plt.plot(x, x**2, label='quadratic')  # etc.
